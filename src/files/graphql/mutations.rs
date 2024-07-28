41a0bbbbc2e3b9a917aac8d656947b1015f4466c
Ezekiel Victor <codeepoch@gmail.com>
2024-07-28 20:13:51 +0100

    create new user if user does not exist

--- src/files/graphql/mutations.rs
+++ src/files/graphql/mutations.rs
@@ -1,8 +1,8 @@
 use async_graphql::*;
-use sea_orm::{ActiveModelTrait, ActiveValue, DbConn};
+use sea_orm::{ActiveModelTrait, ActiveValue, ColumnTrait, DbConn, EntityTrait, QueryFilter};
 
 use crate::services::ipfs::pin_file_to_ipfs;
-use entity::entities::file;
+use entity::entities::{file, user};
 
 use super::types::{inputs::FileInput, outputs::FileType};
 
@@ -13,6 +13,24 @@ pub struct FileMutations;
 impl FileMutations {
     pub async fn upload_file(&self, ctx: &Context<'_>, input: FileInput) -> Result<FileType> {
         let db = ctx.data::<DbConn>()?;
+
+        let user = user::Entity::find()
+            .filter(user::Column::Address.eq(&input.creator_address))
+            .one(db)
+            .await?;
+
+        match user {
+            Some(_) => {}
+            None => {
+                let new_user = user::ActiveModel {
+                    id: ActiveValue::NotSet,
+                    email: ActiveValue::NotSet,
+                    password_hash: ActiveValue::NotSet,
+                    address: ActiveValue::Set(input.creator_address.clone()),
+                };
+                new_user.insert(db).await?;
+            }
+        }
 
         let file_cid = pin_file_to_ipfs(input.file, ctx).await?;
         let nft_cid = pin_file_to_ipfs(input.nft_image, ctx).await?;
